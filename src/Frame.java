31c21f57634d1c17fb31819e3e531a4b59a7cd8b
Mateja Glusac <Mateja Glusac>
2017-03-04 00:11:30 +0000

    Added Panel and Frame classes

---
+++ src/Frame.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+/**
+ * Created by G33 on 04/03/2017.
+ */
+public class Frame extends JFrame {
+}
---
+++ src/Panel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+        import java.awt.event.ActionEvent;
+        import java.awt.event.ActionListener;
+
+/**
+ * Created by G33 on 04/03/2017.
+ */
+public class Panel extends JPanel implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
